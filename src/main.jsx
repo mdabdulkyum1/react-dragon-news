be784d9efa1243fc921f8f2f49189d7b275a871c
mdabdulkyum1 <kyummdabdul@gmail.com>
2024-11-14 17:08:45 +0600

    login sms done :

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,15 @@ import "./index.css";
 import { router } from "./routes/router";
 import AuthProvider from "./AuthProvider/AuthProvider";
 
+
+import { ToastContainer } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
       <RouterProvider router={router} />
     </AuthProvider>
+    <ToastContainer />
   </StrictMode>
 );
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,26 @@
 import { Link, useNavigate } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../AuthProvider/AuthProvider";
+import { toast } from "react-toastify";
 
 function Login() {
+  const [success, setSuccess] = useState("");
+  const [error, setError] = useState(null);
+
+  const notify = () => {
+    toast.success(`Login successfully!!!`, {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  };
+
   const { signInUser } = useContext(AuthContext);
 
   const navigate = useNavigate();
@@ -11,18 +28,22 @@ function Login() {
   const singIn = (e) => {
     e.preventDefault();
 
+    setError(null);
+    setSuccess("");
+
     const email = e.target.email.value;
     const password = e.target.password.value;
     signInUser(email, password)
-      .then((result) => {
-        navigate('/');
-        console.log(result.user);
+      .then(() => {
+        notify();
+        setSuccess("Login successfully!!!");
+        navigate("/");
       })
       .catch((error) => {
-        console.log("ERROR", error.massage);
+        setError(error);
       });
 
-      e.target.reset();
+    e.target.reset();
   };
 
   return (
@@ -62,6 +83,10 @@ function Login() {
               </a>
             </label>
           </div>
+          {success && <p className="text-green-600">{success}</p>}
+          {error && (
+            <p className="text-red-600">Login Failed :( Please Try again...</p>
+          )}
           <div className="form-control mt-6">
             <button className="btn btn-primary">Login</button>
           </div>
